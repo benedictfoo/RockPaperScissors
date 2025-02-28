6bcd6d2d44f0a4db1b42347cb394066753098c75
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-02-28 12:36:31 +0800

    Add Prompt to store user answer

--- script.js
+++ script.js
@@ -15,17 +15,29 @@ function startGame() {
   function makeBotAnswer(acceptableAnswers) {
     return acceptableAnswers[Math.floor(Math.random() * 3)];
   }
-  //   initialize and store in botanswer the result of makebotanswr
-  const botAnswer = makeBotAnswer(acceptableAnswers);
+  // Loop of 5 rounds
+  for (let gameRound = 1; gameRound <= 1; gameRound++) {
+    const botAnswer = makeBotAnswer(acceptableAnswers);
+    //   initialize and store in botanswer the result of makebotanswr
+    // initialize and set a keepGoing boolean variable as true
+    let keepGoing = true;
+    // initialize userAnswer
+    let userAnswer = undefined;
+    // while keepGoing true
+
+    while (keepGoing) {
+      // store string userAnswer in prompt("Rock, Paper or Scissors?")->get this from the A.A. array
+      userAnswer = prompt("Rock, Paper or scissors?").toLowerCase();
+      // if userAnswer lowercased is either of those in A.A array lowerCased keepGoing is False
+      if (acceptableAnswers.includes(userAnswer)) {
+        keepGoing = false;
+      } else {
+        alert("Invalid input. Enter: Rock, Paper or Scissors.");
+      }
+    }
+  }
 }
 
-// Loop of 5 rounds
-// each loop
-// store in new vairable botAnswer the result of makeBotAnswer
-// initialize and set a keepGoing boolean variable as true
-// while keepGoing true
-// store string userAnswer in prompt("Rock, Paper or Scissors?")->get this from the A.A. array
-// if userAnswer lowercased is either of those in A.A array lowerCased keepGoing is False
 // ifbot answer same as userAnswer
 // object.won + 1 to current
 // alert the winner of the round to be user
